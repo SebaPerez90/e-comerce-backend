97685b00e4a968fb7b62b1680e1d2f7cdf4ffe77
Sebastian Perez <112594443+SebaPerez90@users.noreply.github.com>
2024-05-12 14:33:08 -0300

    deploy

--- src/database/config/database.envs.ts
+++ src/database/config/database.envs.ts
@@ -5,6 +5,6 @@ export const PROVIDE = process.env.PROVIDE;
 export const TYPE = 'postgres';
 export const HOST = process.env.HOST;
 export const PORT = Number(process.env.PORT);
-export const USERNAME = 'postgres';
+export const USERNAME = 'postgresdb';
 export const PASSWORD = process.env.PASSWORD;
 export const DATABASE = process.env.DATABASE;
